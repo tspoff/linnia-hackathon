5066a1485f9edbe2e746582948316800c32f4ee6
Dev <dev@MacBook-Pro-2.local>
2018-08-26 05:56:31 -0400

    contract fixes & deployment

--- contracts/DataPayments.sol
+++ contracts/DataPayments.sol
@@ -19,6 +19,7 @@ contract DataPayments {
     mapping (address => bool) public hasPayoutGroup;
     address[] public buyers;
     
+    /** Events */
     event Deposited(address indexed payee, uint256 weiAmount);
     event Withdrawn(address indexed payee, uint256 weiAmount);
     event PayoutDistributed(address indexed buyer, uint256 weiAmount);
@@ -48,7 +49,9 @@ contract DataPayments {
         for (uint i = 0; i < _users.length; i++) {
             payoutGroups[msg.sender].users[i] = _users[i];
             payoutGroups[msg.sender].weights[i] = _weights[i];
-            payoutGroups[msg.sender].totalWeight.add(_weights[i]);
+            
+            uint totalWeight = payoutGroups[msg.sender].totalWeight;
+            payoutGroups[msg.sender].totalWeight = totalWeight.add(_weights[i]);
         }
     
         payoutGroups[msg.sender].length = _users.length;
@@ -58,18 +61,27 @@ contract DataPayments {
     * @dev Deposits payments accourding to PayoutGroup
     * You have to be registered as a buyer to deposit funds
     */
-    function depositPayout() public payable onlyRegisteredBuyers() {
+    function depositPayout() public payable {
         
-        uint totalValue = msg.value;
+        require(msg.value > 0);
+        
+        uint256 totalWeight = payoutGroups[msg.sender].totalWeight;
+        
+        address user;
+        uint256 weight;
+        uint value;
         
         for (uint i = 0; i < payoutGroups[msg.sender].length; i++) {
-            address user = payoutGroups[msg.sender].users[i];
-            uint256 weight = payoutGroups[msg.sender].weights[i];
-            
-            _deposit(user, totalValue / weight);
+            user = payoutGroups[msg.sender].users[i];
+            weight = payoutGroups[msg.sender].weights[i];
+
+            // Give each user a weighted proportion of the value
+            // NOTE: This truncates presicion as there is no floating point math
+            value = msg.value / totalWeight * weight;
+            _deposit(user, value);
         }
-        
-        emit PayoutDistributed(msg.sender, totalValue);
+
+        emit PayoutDistributed(msg.sender, msg.value);
     }
 
     //
@@ -82,7 +94,8 @@ contract DataPayments {
     * @param _payee The destination address of the funds.
     */
     function _deposit(address _payee, uint256 _value) internal {
-        deposits[_payee] = deposits[_payee].add(_value);
+        uint256 deposit = deposits[_payee];
+        deposits[_payee] = deposit.add(_value);
         emit Deposited(_payee, _value);
     }
 
@@ -93,7 +106,9 @@ function withdraw() public {
     require(deposits[msg.sender] > 0);
     
     uint256 payment = deposits[msg.sender];
+    
     assert(address(this).balance >= payment);
+    
     deposits[msg.sender] = 0;
     emit Withdrawn(msg.sender, payment);
     
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,11 +3,14 @@ const LinniaUsers = artifacts.require("./LinniaUsers.sol")
 const LinniaRecords = artifacts.require("./LinniaRecords.sol")
 const LinniaPermissions = artifacts.require("./LinniaPermissions.sol")
 const DataPayments = artifacts.require("./DataPayments.sol")
+const config = require('../config.js');
 
 module.exports = (deployer, network, accounts) => {
   const adminAddress = accounts[0];
-  let hubInstance;
+  let hubInstance, dataInstance;
   deployer.deploy(DataPayments);
+
+  let payoutGroups = config.payoutGroups;
 
   // deploy the hub
   deployer.deploy(LinniaHub).then(() => {
@@ -25,12 +28,27 @@ module.exports = (deployer, network, accounts) => {
   }).then(() => {
     // set all the addresses in the hub
     return deployer.deploy(DataPayments)
-  }).then(() => {
+  }).then((_dataInstance) => {
+    dataInstance = _dataInstance;
     // set all the addresses in the hub
     return hubInstance.setUsersContract(LinniaUsers.address)
   }).then(() => {
     return hubInstance.setRecordsContract(LinniaRecords.address)
   }).then(() => {
     return hubInstance.setPermissionsContract(LinniaPermissions.address)
+  }).then(() => {
+    return dataInstance.registerAsBuyer({from: accounts[0]})
+  }).then(() => {
+    return dataInstance.registerAsBuyer({from: accounts[1]})
+  }).then(() => {
+    return dataInstance.registerAsBuyer({from: accounts[2]})
+  }).then(() => {
+    return dataInstance.setPayoutGroup(payoutGroups[0].users, payoutGroups[0].weights, {from: accounts[0]})
+  }).then(() => {
+    return dataInstance.setPayoutGroup(payoutGroups[0].users, payoutGroups[0].weights, {from: accounts[1]})
+  }).then(() => {
+    return dataInstance.setPayoutGroup(payoutGroups[0].users, payoutGroups[0].weights, {from: accounts[2]})
   })
+  
+  ;
 }
