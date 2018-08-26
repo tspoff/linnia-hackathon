ce8dd6817ac3c4cb5054890b9dba7b1bbfdf4f41
Tin Buzancic <TinBuzancic@email.com>
2018-08-26 17:47:22 -0400

    added missing documentation for functions in DataPayments.sol

--- contracts/DataPayments.sol
+++ contracts/DataPayments.sol
@@ -2,75 +2,84 @@ pragma solidity ^0.4.24;
 
 import "./math/SafeMath.sol";
 
+/** Start */
 contract DataPayments {
-
+    /** Dependancies */
     using SafeMath for uint256;
-    
-    /** Contract Variables */
+
+    /** Smart Contract Variables */
     struct PayoutGroup {
         mapping (uint256 => address) users;
         mapping (uint256 => uint256) weights;
         uint256 length;
         uint256 totalWeight;
     }
-    
-    mapping (address => uint256) private deposits; 
+
+    /** Smart Contract Mappings */
+    mapping (address => uint256) private deposits;
     mapping (address => PayoutGroup) public payoutGroups;
     mapping (address => bool) public hasPayoutGroup;
     address[] public buyers;
-    
-    /** Events */
+
+    /** Smart Contract Events */
     event Deposited(address indexed payee, uint256 weiAmount);
     event Withdrawn(address indexed payee, uint256 weiAmount);
     event PayoutDistributed(address indexed buyer, uint256 weiAmount);
-    
-    /** Modifiers */
+
+    /** Smart Contract Modifiers */
     modifier onlyRegisteredBuyers() {
         require(hasPayoutGroup[msg.sender]);
         _;
     }
 
-    /** Functions */
-    
+    /** Start of Functions */
+    /***********************/
+
     /**
     * @dev Registers the caller as a buyer
-    * For now anyone can register, this could be expanded to have qualifications;
+    * For now anyone can register, this would ideally be expanded to have qualifications
     */
     function registerAsBuyer() public {
         hasPayoutGroup[msg.sender] = true;
         buyers.push(msg.sender);
     }
-     
+
+    /**
+    * @dev Gets users and the weight of each Users data and aggregates them into a Payout group
+    * @param _users - Adresses of the Users to Pay payout
+    * @param _weights - diversifies the value of each users' data in the Pay Out Group
+    */
     function setPayoutGroup(address[] _users, uint256[] _weights) public onlyRegisteredBuyers() {
         require(_users.length == _weights.length);
-         
+
         hasPayoutGroup[msg.sender] = true;
-    
+
+        //Assign the Users and Weights to the Payoutgroup
         for (uint i = 0; i < _users.length; i++) {
             payoutGroups[msg.sender].users[i] = _users[i];
             payoutGroups[msg.sender].weights[i] = _weights[i];
-            
+
             uint totalWeight = payoutGroups[msg.sender].totalWeight;
             payoutGroups[msg.sender].totalWeight = totalWeight.add(_weights[i]);
         }
-    
+
         payoutGroups[msg.sender].length = _users.length;
     }
 
     /**
-    * @dev Deposits payments accourding to PayoutGroup
-    * You have to be registered as a buyer to deposit funds
+    * @dev Deposits payments according to the values returned in PayoutGroup
+    * You must be registered as a buyer to deposit funds
     */
     function depositPayout() public payable {
-        
+
         require(msg.value > 0);
-        
+
         uint256 totalWeight = payoutGroups[msg.sender].totalWeight;
-        
+
         address user;
         uint256 weight;
         uint value;
-        
+
         for (uint i = 0; i < payoutGroups[msg.sender].length; i++) {
             user = payoutGroups[msg.sender].users[i];
             weight = payoutGroups[msg.sender].weights[i];
@@ -84,11 +93,13 @@ contract DataPayments {
         emit PayoutDistributed(msg.sender, msg.value);
     }
 
-    //
+    /*
+    * returns an unsigned int of the Payees deposit
+    */
     function depositsOf(address _payee) public view returns (uint256) {
         return deposits[_payee];
     }
-    
+
     /**
     * @dev Stores the sent amount as credit to be withdrawn.
     * @param _payee The destination address of the funds.
@@ -100,18 +111,18 @@ contract DataPayments {
     }
 
 /**
-* @dev Withdraw accumulated balance for a payee.
+* @dev Withdraw accumulated balance of an individual Payee.
 */
 function withdraw() public {
     require(deposits[msg.sender] > 0);
-    
+
     uint256 payment = deposits[msg.sender];
-    
+
     assert(address(this).balance >= payment);
-    
+
     deposits[msg.sender] = 0;
     emit Withdrawn(msg.sender, payment);
-    
+
     msg.sender.transfer(payment);
     }
-} 
+}
